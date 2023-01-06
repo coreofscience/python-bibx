d5186dae587af593f5ad3d8a6c2e70e07bc7a02a
Oscar Arbelaez <oscar@arbelaez.dev>
2023-01-06 08:50:49 -0500

    Add label doi and cache

--- src/bibx/entities/article.py
+++ src/bibx/entities/article.py
@@ -12,7 +12,28 @@ class Article:
     issue: Optional[str] = None
     page: Optional[str] = None
     doi: Optional[str] = None
+    _label: Optional[str] = None
     references: Optional[List["Article"]] = field(default_factory=list)
     keywords: Optional[List[str]] = field(default_factory=list)
     sources: Optional[Set[str]] = field(default_factory=set)
     extra: Optional[Mapping] = field(default_factory=dict)
+
+    @property
+    def key(self):
+        author = self.authors[0].split(" ")[0].replace(",", "")
+        year = self.year
+        return f"{author}{year}".lower()
+
+    @property
+    def label(self):
+        if self._label is not None:
+            return self._label
+        pieces = {
+            "AU": self.authors[0].replace(",", ""),
+            "PY": str(self.year),
+            "J9": str(self.journal),
+            "VL": f"V{self.volume}" if self.volume else None,
+            "BP": f"P{self.page}" if self.page else None,
+            "DI": f"DOI {self.doi}" if self.doi else None,
+        }
+        return ", ".join(value for value in pieces.values() if value)
--- src/bibx/entities/collection.py
+++ src/bibx/entities/collection.py
@@ -1,7 +1,10 @@
+import logging
 from dataclasses import dataclass
 from typing import Iterable, List, Tuple
 
 from bibx.entities.article import Article
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -10,8 +13,14 @@ class Collection:
 
     @property
     def citation_pairs(self) -> Iterable[Tuple[Article, Article]]:
+        cache = {article.key: article for article in self.articles}
         for article in self.articles:
             if not article.references:
                 continue
             for reference in article.references:
-                yield (article, reference)
+                # Yield a full article if we have the metadata for a citation
+                if article.key != reference.key and reference.key in cache:
+                    logger.debug("Found a cache hit for key %s", reference.key)
+                    yield (article, cache[reference.key])
+                else:
+                    yield (article, reference)
