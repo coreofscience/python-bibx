1741a35afc1a55182478893b034224eaf646541c
Oscar Arbelaez <oscar@arbelaez.dev>
2023-03-09 15:27:09 +0000

    Run pre-commit

--- src/bibx/entities/collection_builders/isi.py
+++ src/bibx/entities/collection_builders/isi.py
@@ -4,14 +4,16 @@ import logging
 import re
 from contextlib import suppress
 from dataclasses import dataclass
-from typing import (Any, Callable, Dict, Iterable, List, Mapping, Optional,
-                    TextIO)
+from typing import Any, Callable, Dict, Iterable, List, Mapping, Optional, TextIO
 
 from bibx.entities.article import Article
 from bibx.entities.collection import Collection
 from bibx.entities.collection_builders.base import CollectionBuilder
-from bibx.exceptions import (InvalidIsiLineError, InvalidIsiReference,
-                             MissingCriticalInformation)
+from bibx.exceptions import (
+    InvalidIsiLineError,
+    InvalidIsiReference,
+    MissingCriticalInformation,
+)
 
 logger = logging.getLogger(__name__)
 
