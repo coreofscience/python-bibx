f3a335fd92771867d6204566e6543c994a27e583
Oscar Arbelaez <oscar@arbelaez.dev>
2025-02-15 18:46:07 +0000

    Add functional test

---
+++ tests/builders/test_scopus_csv.py
@@ -0,0 +1,8 @@
+from bibx import read_scopus_csv
+
+
+def test_scopus_csv() -> None:
+    """Test the ScopusCSVBuilder class."""
+    with open("docs/examples/scopus.csv") as file:
+        collection = read_scopus_csv(file)
+    assert collection is not None
